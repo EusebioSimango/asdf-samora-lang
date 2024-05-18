98225216d5018ffd392d99588858dabfd86cb496
Eusebio Simango <eusebiosimango14@gmail.com>
2024-05-18 15:38:34 +0200

    chore: test

--- lib/utils.bash
+++ lib/utils.bash
@@ -14,11 +14,6 @@ fail() {
 
 curl_opts=(-fsSL)
 
-# NOTE: You might want to remove this if samora-lang is not hosted on GitHub releases.
-if [ -n "${GITHUB_API_TOKEN:-}" ]; then
-	curl_opts=("${curl_opts[@]}" -H "Authorization: token $GITHUB_API_TOKEN")
-fi
-
 sort_versions() {
 	sed 'h; s/[+-]/./g; s/.p\([[:digit:]]\)/.z\1/; s/$/.z/; G; s/\n/ /' |
 		LC_ALL=C sort -t. -k 1,1 -k 2,2n -k 3,3n -k 4,4n -k 5,5n | awk '{print $2}'
@@ -41,8 +36,11 @@ download_release() {
 	version="$1"
 	filename="$2"
 
-	# TODO: Adapt the release URL convention for samora-lang
-	url="$GH_REPO/archive/v${version}.tar.gz"
+	# https://github.com/GraHms/Samora-Lang/releases/download/v0.2.0/samora-lang_0.2.0_linux_amd64.tar.gz
+
+	platform=$(uname -s | tr '[:upper:]' '[:lower:]')
+
+	url="$GH_REPO/releases/download/v${version}/samora-lang_${version}_${platform}_amd64.tar.gz"
 
 	echo "* Downloading $TOOL_NAME release $version..."
 	curl "${curl_opts[@]}" -o "$filename" -C - "$url" || fail "Could not download $url"
@@ -53,6 +51,8 @@ install_version() {
 	local version="$2"
 	local install_path="${3%/bin}/bin"
 
+	echo "$install_type"
+
 	if [ "$install_type" != "version" ]; then
 		fail "asdf-$TOOL_NAME supports release installs only"
 	fi
@@ -61,7 +61,8 @@ install_version() {
 		mkdir -p "$install_path"
 		cp -r "$ASDF_DOWNLOAD_PATH"/* "$install_path"
 
-		# TODO: Assert samora-lang executable exists.
+		mv "${install_path}/${TOOL_NAME}"  "${install_path}/samora"
+
 		local tool_cmd
 		tool_cmd="$(echo "$TOOL_TEST" | cut -d' ' -f1)"
 		test -x "$install_path/$tool_cmd" || fail "Expected $install_path/$tool_cmd to be executable."
